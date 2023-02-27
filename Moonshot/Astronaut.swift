23e522e3dfaebd0befc7e814118a6fba10e7b9c9
Matheus <mullerscalzer@hotmail.com>
2023-02-27 19:08:32 -0300

    Initial Commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Matheus Müller on 23/02/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
